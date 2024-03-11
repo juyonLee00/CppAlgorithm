a2c88e9d8e8ef1906ef19c9c854cae2599464d92
juyonLee00 <ljy012900@gmail.com>
2024-03-11 19:38:48 +0900

    [week1] bj_15552

---
+++ Week_1/bj_15552.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t, a, b;
+    cin >> t;
+    while(t--)
+    {
+        cin >> a >> b;
+        cout << a+b << '\n';
+    }
+    return 0;
+}
