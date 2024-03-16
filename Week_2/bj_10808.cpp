c58fafd13cef2d7d4c579158c1b20b96c3927587
juyonLee00 <ljy012900@gmail.com>
2024-03-16 21:59:35 +0900

    [week2] bj_10808

---
+++ Week_2/bj_10808.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int arr[26] = {0};
+
+    string s;
+    cin >> s;
+
+    for(int i=0; i<s.size(); i++)
+    {
+        arr[s[i]-'a']+=1;
+    }
+
+    for(int i=0; i<26; i++)
+        cout << arr[i] << " ";
+    return 0;
+}
